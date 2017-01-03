5f19dba350dcbf93c8239ed8050ee49706011a04
proprefenetre <proprefenetre@gmail.com>
2017-01-03 13:38:23 +0100

    add rmlink alias

--- link.sh
+++ link.sh
@@ -7,10 +7,12 @@ dest_config=(libinput-gestures.conf redshift.conf gtk-3.0 i3 dunst)
 for f in ${dest_home[@]}; do
     cd $HOME
     ln -s "$df_dir/$f" ".$f"
+    echo "linked $df_dir/$f --> .$f"
 done
 
 for f in ${dest_config[@]}; do
     [[ ! -d "$HOME/.config" ]] && mkdir "$HOME/.config"
     cd $HOME
     ln -s "$df_dir/$f" ".config/$f"
+    echo "linked $df_dir/$f --> .config/$f"
 done
