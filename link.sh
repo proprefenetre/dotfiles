00511b967d994057f9754f28b761e40e23b1e352
proprefenetre <proprefenetre@gmail.com>
2017-01-13 09:01:05 +0100

    tweaks

--- link.sh
+++ link.sh
@@ -6,13 +6,11 @@ dest_config=(libinput-gestures.conf redshift.conf gtk-3.0 i3 dunst)
 
 for f in ${dest_home[@]}; do
     cd $HOME
-    ln -s "$df_dir/$f" ".$f"
-    echo "linked $df_dir/$f --> .$f"
+    ln -s "$df_dir/$f" ".$f" && echo "linked $df_dir/$f --> .$f"
 done
 
 for f in ${dest_config[@]}; do
     [[ ! -d "$HOME/.config" ]] && mkdir "$HOME/.config"
     cd $HOME
-    ln -s "$df_dir/$f" ".config/$f"
-    echo "linked $df_dir/$f --> .config/$f"
+    ln -s "$df_dir/$f" ".config/$f" && echo "linked $df_dir/$f --> .config/$f"
 done
