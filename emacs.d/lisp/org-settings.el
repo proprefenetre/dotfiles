0d93559ceb65094bd9ae33662a1def3a307a3430
proprefenetre <proprefenetre@gmail.com>
2018-04-07 21:16:05 +0200

    removed org-cycle-hide-drawers

--- emacs.d/lisp/org-settings.el
+++ emacs.d/lisp/org-settings.el
@@ -40,45 +40,8 @@
   (set-face-attribute 'org-level-5 nil :foreground "#87d7ff")
   (set-face-attribute 'org-level-6 nil :foreground "#5fffaf"))
 
-(defun org-cycle-hide-drawers (state)
-  "Re-hide all drawers after a visibility STATE change."
-  (when (and (derived-mode-p 'org-mode)
-             (not (memq state '(overview folded contents))))
-    (save-excursion
-      (let* ((globalp (memq state '(contents all)))
-             (beg (if globalp
-                      (point-min)
-                    (point)))
-             (end (if globalp
-                      (point-max)
-                    (if (eq state 'children)
-                        (save-excursion
-                          (outline-next-heading)
-                          (point))
-                      (org-end-of-subtree t)))))
-        (goto-char beg)
-        (while (re-search-forward org-drawer-regexp end t)
-          (save-excursion
-            (beginning-of-line 1)
-            (when (looking-at org-drawer-regexp)
-              (let* ((start (1- (match-beginning 0)))
-                     (limit
-                      (save-excursion
-                        (outline-next-heading)
-                        (point)))
-                     (msg (format
-                           (concat
-                            "org-cycle-hide-drawers:  "
-                            "`:END:`"
-                            " line missing at position %s")
-                           (1+ start))))
-                (if (re-search-forward "^[ \t]*:END:" limit t)
-                    (outline-flag-region start (point-at-eol) t)
-                  (user-error msg))))))))))
-
 (eval-after-load "org" '(progn (pfn-org-level-colors)
                                (pfn-org-level-sizes)))
-                               ;; (org-cycle-hide-drawers 'children)))
 
 ;; Ensure ELPA org is prioritized above built-in org.
 (require 'cl)
