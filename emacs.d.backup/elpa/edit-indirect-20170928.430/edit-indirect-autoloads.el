0fdf09197e2e2942e4a41c7e89ba3824f8b24435
proprefenetre <proprefenetre@gmail.com>
2018-04-13 17:54:15 +0200

    moved the backup

--- emacs.d.backup/elpa/edit-indirect-20170928.430/edit-indirect-autoloads.el
+++
@@ -1,25 +0,0 @@
-;;; edit-indirect-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "edit-indirect" "edit-indirect.el" (0 0 0 0))
-;;; Generated autoloads from edit-indirect.el
-
-(autoload 'edit-indirect-region "edit-indirect" "\
-Edit the region BEG..END in a separate buffer.\nThe region is copied, without text properties, to a separate\nbuffer, called edit-indirect buffer, and\n`edit-indirect-guess-mode-function' is called to set the major\nmode.\nWhen done, exit with `edit-indirect-commit', which will remove the\noriginal region and replace it with the edited version; or with\n`edit-indirect-abort', which will drop the modifications.\n\nThis differs from `clone-indirect-buffer' with narrowing in that\nthe text properties are not shared, so the parent buffer major mode\nand the edit-indirect buffer major mode will not be able to tread\non each other's toes by setting up potentially conflicting text\nproperties, which happens surprisingly often when the font-lock\nmode is used.\n\nEdit-indirect buffers use the `edit-indirect-mode-map' keymap.\n\nIf there's already an edit-indirect buffer for BEG..END, use that.\nIf there's already an edit-indirect buffer active overlapping any\nportion of BEG..END, an `edit-indirect-overlapping' error is\nsignaled.\n\nWhen DISPLAY-BUFFER is non-nil or when called interactively,\ndisplay the edit-indirect buffer in some window and select it.\n\nIn any case, return the edit-indirect buffer.\n\n(fn BEG END &optional DISPLAY-BUFFER)" t nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "edit-indirect" '("edit-indirect-")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; edit-indirect-autoloads.el ends here
--- emacs.d.backup/elpa/minions-20180321.749/minions-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "minions" "20180321.749" "A minor-mode menu for the mode line" '((emacs "25.3") (dash "2.13.0")) :commit "acac7fb0b04ffdf243775231690956d163474177" :url "https://github.com/tarsius/minions")
