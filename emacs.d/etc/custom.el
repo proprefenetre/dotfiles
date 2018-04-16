63711191aa7003b6c136e11739e1c265c22a8f6e
proprefenetre <proprefenetre@gmail.com>
2018-04-16 10:21:18 +0200

    something something automatically

--- emacs.d/etc/custom.el
+++ emacs.d/etc/custom.el
@@ -3,8 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(no-littering yasnippet smart-mode-line nord-theme challenger-deep-theme key-chord general markdown-mode olivetti hydra counsel which-key shackle eyebrowse rainbow-mode rainbow-delimiters magit racket-mode elpy python-mode rust-mode yaml-mode aggressive-indent paredit avy org-plus-contrib evil-commentary evil-embrace evil-surround evil-collection evil use-package)))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
