3eaffb3078b9bd520ce68b5fabbef553b9bd5cab
proprefenetre <proprefenetre@gmail.com>
2018-05-10 23:06:52 +0200

    remove that global org-capture shit

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -379,42 +379,6 @@
           ("s" "scriptie note" entry (file+headline "~/projects/thesis/todo.org" "Notes") "* %?")
           ("S" "scriptie todo" entry (file+headline "~/projects/thesis/todo.org" "To Do") "* TODO %?"))))
 
-;; global org-capture
-(defadvice org-capture-finalize
-    (after delete-capture-frame activate)
-  "Advise capture-finalize to close the frame."
-  (if (equal "capture" (frame-parameter nil 'name))
-      (delete-frame)))
-
-(defadvice org-capture-destroy
-    (after delete-capture-frame activate)
-  "Advise capture-destroy to close the frame."
-  (if (equal "capture" (frame-parameter nil 'name))
-      (delete-frame)))
-
-;; make the frame contain a single window. by default org-capture
-;; splits the window.
-(add-hook 'org-capture-mode-hook
-          'delete-other-windows)
-
-(defadvice org-switch-to-buffer-other-window
-    (after supress-window-splitting activate)
-  "Delete the extra window if we're in a capture frame."
-  (if (equal "capture" (frame-parameter nil 'name))
-      (delete-other-windows)))
-
-(defun make-capture-frame ()
-  "Create a new frame and run 'org-capture'."
-  (interactive)
-  (make-frame '((name . "capture")
-                (width . 120)
-                (height . 15)))
-  (select-frame-by-name "capture")
-  (setq word-wrap 1)
-  (setq truncate-lines nil)
-  (org-capture))
-
-
 (use-package org-pdfview
   :demand t
   :after org
@@ -422,7 +386,6 @@
   (add-to-list 'org-file-apps
                '("\\.pdf\\'" . (lambda (file link)
                                  (org-pdfview-open link)))))
-
 
 (use-package paredit
   :demand t
