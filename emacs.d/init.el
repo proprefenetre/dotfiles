cda01156a095b843d24980030369acd7a44485ff
proprefenetre <proprefenetre@gmail.com>
2019-01-03 17:45:33 +0100

    new snippets; tweak init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,4 @@
-;;; init.el --- Emacs Configuration
+;; init.el --- Emacs Configuration
 ;;; Commentary:
 ;;; Tuesday 09 iteration: a new hope
 ;;; Code:
@@ -29,6 +29,7 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
 (require 'use-package)
 
 (setq use-package-always-defer t
@@ -44,7 +45,8 @@
 (load custom-file)
 
 ;;; personal lisp collection
-(add-to-list 'load-path "/home/niels/dotfiles/emacs.d/var/lisp")
+;; (add-to-list 'load-path "/home/niels/dotfiles/emacs.d/var/lisp")
+(add-to-list 'load-path "/home/niels/dotfiles/emacs.d")
 
 (require 'custom-functions)
 
@@ -65,6 +67,8 @@
 ;; "#ff5458"
 ;; "#ffe9aa"
 ;; "#95ffa4"
+;; "#aaffe4"
+;; "#ff8080"
 
 (set-face-attribute 'default nil :font "Iosevka 11")
 (set-face-attribute 'line-number nil :background 'unspecified)
@@ -77,9 +81,14 @@
 (prefer-coding-system 'utf-8)
 (setq default-input-method "latin-postfix")
 
+(require 'recentf)
 (setq-default abbrev-mode 1
               recentf-mode 1
-	          show-paren-mode 1)
+	          show-paren-mode 1
+              electric-pair-mode 1)
+
+(dolist (table abbrev-table-name-list)
+  (abbrev-table-put (symbol-value table) :case-fixed t))
 
 (run-at-time nil (* 5 60)
              (lambda ()
@@ -99,7 +108,9 @@
 
 (setq-default indent-tabs-mode nil
               tab-width 4
-	          fill-column 80)
+	          fill-column 80
+              scroll-margin 20
+              scroll-conservatively most-positive-fixnum)
 
 (setq-default auto-fill-function 'do-auto-fill)
 
@@ -128,7 +139,7 @@
 
 (setq frame-title-format "%b")
 
-(setq compilation-read-command nil)
+;; (setq compilation-read-command nil)
 
 (setq hippie-expand-try-functions-list
       '(try-complete-file-name-partially
@@ -137,21 +148,46 @@
         try-expand-dabbrev-all-buffers
         try-expand-dabbrev-from-kill))
 
-;;; built-ins
-(require 'recentf)
-(recentf-mode 1)
-(require 'table)
-
 ;;; Packages
+(use-package s)
+
+(use-package prescient
+  :demand t
+  :config
+  (prescient-persist-mode 1)
+  (setq ivy-prescient-excluded-commands '(swiper evil-search-function)))
+
+(use-package ivy-prescient
+  :after prescient
+  :demand t
+  :config
+  (ivy-prescient-mode 1))
+
+(use-package company-prescient
+  :after prescient
+  :demand t
+  :config
+  (company-prescient-mode 1))
+
+(use-package projectile
+  :demand t
+  :config
+  (projectile-mode))
+
+(use-package counsel-projectile
+  :demand t
+  :config
+  (counsel-projectile-mode))
+
 (use-package smex
   :demand t)
+
+(use-package poly-markdown)
 
 (use-package polymode
   :mode
   ("\\.Rnw" . poly-noweb+r-mode)
   ("\\.Rmd" . poly-markdown+r-mode))
-
-(use-package poly-markdown)
 
 (use-package ess
   :config
@@ -187,9 +223,17 @@
 (use-package company
   :demand t
   :config
-  (setq company-idle-delay 0.3
-        company-selection-wrap-around t)
-  (delete 'company-dabbrev company-backends))
+  (setq company-idle-delay 0
+        company-selection-wrap-around t
+        company-require-match 'never)
+  (setq company-backends
+        '(company-files
+          (company-yasnippet
+           company-keywords
+           company-capf)
+          (company-dabbrev-code
+           company-dabbrev
+           company-abbrev))))
 
 (use-package counsel
   :demand t)
@@ -207,11 +251,16 @@
         evil-vsplit-window-right t
         evil-split-window-below t
         evil-cross-lines t)
+  (defadvice evil-inner-word (around underscore-as-word activate)
+    (let ((table (copy-syntax-table (syntax-table))))
+      (modify-syntax-entry ?_ "w" table)
+      (with-syntax-table table
+        ad-do-it)))
   (evil-mode 1))
 
 (use-package evil-collection
-  :demand t
   :after evil
+  :demand t
   :init (setq evil-collection-outline-bind-tab-p nil
 	          evil-collection-setup-minibuffer t)
   :config (evil-collection-init))
@@ -268,15 +317,27 @@
 (use-package magit
   :config (setq magit-completing-read-function 'ivy-completing-read))
 
+(use-package evil-magit
+  :after '(evil magit)
+  :config
+  (setq evil-magit-state 'normal))
+
 (use-package markdown-mode
   :mode
-  ("\\.md" . markdown-mode)
+  ;; ("\\.md" . markdown-mode)
   ("\\.mdpp" . markdown-mode)
   :init
   (add-hook 'markdown-mode-hook 'turn-on-olivetti-mode)
   :config
   (font-lock-add-keywords 'markdown-mode
-                          '(("@[[:alnum:]]+\\(-[[:alnum:]]+\\)?" . font-lock-keyword-face))))
+                          '(("@[^ ]+"
+                             . font-lock-variable-name-face)))
+  (font-lock-add-keywords 'markdown-mode
+                          '(("\\(@fig:\\|@tbl:\\)\\([^ ]+\\)"
+                             (1 font-lock-keyword-face) (2 font-lock-variable-name-face))))
+  (font-lock-add-keywords 'markdown-mode
+                          '(("(b?red .*)" . font-lock-keyword-face)))
+  )
 
 (use-package olivetti
   :config (setq-default olivetti-body-width 90))
@@ -297,20 +358,22 @@
         org-refile-allow-creating-parent-nodes t
         org-refile-use-outline-path t
         org-archive-location "~/org/archief::datetree/"
-	org-cycle-separator-lines -1
-	org-blank-before-new-entry '((heading . nil)
-				     (plain-list-item . nil))
-	org-M-RET-may-split-line '((default . nil))
+	    org-cycle-separator-lines -1
+	    org-blank-before-new-entry '((heading . nil)
+				                     (plain-list-item . nil))
+	    org-M-RET-may-split-line '((default . nil))
         org-return-follows-link t
-	org-reverse-note-order t
+	    org-reverse-note-order t
         org-outline-path-complete-in-steps nil)
 
   (setq org-todo-keyword-faces
         '(("READ" . "#65b2ff")
-          ("GOOGLE" . "#65b2ff")))
+          ("GOOGLE" . "#65b2ff")
+          ("AFSPRAAK" . "#906cff")
+          ("WEEKLY" . "#aaffe4")))
 
   (setq org-capture-templates
-	'(("d" "dict" entry (file+headline "~/org/dict.org" "Words") "* %? :: "))))
+	    '(("d" "dict" entry (file+headline "~/org/dict.org" "Words") "* %? :: "))))
 
 (use-package paredit
   :demand t
@@ -323,6 +386,21 @@
 
 (use-package rainbow-mode)
 
+;; (setq mode-line-format
+;;       (list
+;;        " "
+;;        mode-line-mule-info
+;;        'mode-line-modified
+;;        "-  "
+;;        'mode-line-buffer-identification
+;;        ;; 'mode-line-modes
+;;        " -- "
+;;        `(vc-mode vc-mode)
+
+;;        ;; Fill until the end of line but 10 characters
+;;        (mode-line-fill 'mode-line 10)
+;;        "(%l, %c)"))
+
 (use-package smart-mode-line
   :demand t
   :config
@@ -331,16 +409,14 @@
   (setq sml/theme 'respectful)
   (setq sml/modified-char "+")
   (setq sml/name-width 40)
-  (setq sml/shorten-modes nil)
-  (setq sml/mode-width 0)
+  (setq sml/mode-width 'full)
   (setq rm-whitelist
-        (format "^ \\(%s\\)$"
-                (mapconcat #'identity
-                           '("()" "Fly" "PRJ.*" "=>")
-                           "\\|")))
-  (add-to-list 'sml/replacer-regexp-list '("^~/projects/thesis" ":TH:") t)
-  (add-to-list 'sml/replacer-regexp-list '("^~/projects/" ":PRJ:") t)
-  (add-to-list 'sml/replacer-regexp-list '("^~/dotfiles" ":DF:"))
+        (format "^ \\(%s\\)$" (mapconcat #'identity
+                                         '("()" "Fly" "PRJ.*" "=>") "\\|")))
+  (add-to-list 'sml/replacer-regexp-list '("^~/projects/thesis" ":thesis:") t)
+  (add-to-list 'sml/replacer-regexp-list '("^~/projects/" ":projects:") t)
+  (add-to-list 'sml/replacer-regexp-list '("^~/dotfiles" ":df:"))
+  (add-to-list 'sml/replacer-regexp-list '("^~/org" ":org:"))
   (sml/setup))
 
 (use-package which-key
@@ -363,34 +439,38 @@
   (setq yas-triggers-in-field t
         yas-snippet-revival t
         yas-indent-line 'nil
-        yas-wrap-around-region t)
+        yas-wrap-around-region t
+        yas-indent-line 'auto
+        yas-also-auto-indent-first-line t)
   (yas-global-mode 1))
 
-;;; Hooks
-
+;;; Hooks & Builtins
+(require 'table)
 (add-hook 'focus-out-hook 'garbage-collect)
+(add-hook 'sh-mode-hook 'aggressive-indent-mode)
 
 (defun pfn-setup-prog-mode ()
   "Load 'prog-mode' minor modes."
   (eldoc-mode 1)
-  (company-mode-on)
-  (aggressive-indent-mode)
+  (auto-fill-mode)
   (rainbow-delimiters-mode)
-  (display-line-numbers-mode)
   (delete-trailing-whitespace)
   (flycheck-mode 1)
-  (outline-minor-mode))
+  (outline-minor-mode)
+  (company-mode-on)
+  (display-line-numbers-mode))
 (add-hook 'prog-mode-hook 'pfn-setup-prog-mode)
 
 (defun pfn-setup-text-mode ()
   "Load 'text-mode' hooks."
   (delete-trailing-whitespace)
+  (turn-on-auto-fill)
   (rainbow-delimiters-mode 1)
   (flyspell-mode 1))
 (add-hook 'text-mode-hook 'pfn-setup-text-mode)
 
 ;;; Keys
-;; (setq tab-always-indent 'complete)
+(setq tab-always-indent 'complete)
 
 (general-evil-setup)
 
@@ -406,11 +486,12 @@
   (general-chord "jj") 'evil-normal-state)
 
 (general-def 'normal (ibuffer-mode-map image-mode-map inferior-ess-r-mode-map)
-  "," nil)
+  "," nil
+  "C-c C-w" 'eyebrowse-last-window-config)
 
 (general-def 'insert markdown-mode-map
-  "TAB" 'company-complete-common-or-cycle
-  "<tab>" 'company-complete-common-or-cycle)
+  "TAB"   'yas-expand
+  "<tab>" 'yas-expand)
 
 (dolist (key '("<return>" "RET"))
   (general-def company-active-map key
@@ -419,15 +500,15 @@
 			               (when (company-explicit-action-p)
 			                 cmd)))))
 
-(general-def company-active-map
-  "TAB" 'company-complete-selection
+(general-def 'insert company-active-map
+  "TAB" 'company-complete-common-or-cycle
   "SPC" nil
   "C-w" 'evil-delete-backward-word)
 
 (general-def '(normal visual emacs)
   :prefix ","
   :non-normal-prefix "M-,"
-  :keymaps 'override
+  :keymaps '(override inferior-ess-r-mode-map)
   "b" 'switch-to-previous-buffer
   "d" 'dired-jump
   "e" 'eval-last-sexp
@@ -440,12 +521,12 @@
 	     (revert-buffer :ignore-auto :noconfirm))
   "R" '(lambda () (interactive)
 	     (load-file user-init-file)
-	     (message "buffer reloaded"))
+         (message "buffer reloaded"))
   "s" 'magit-status)
 
 (general-def
   :prefix "C-c"
-  "a"   'evil-numbers/inc-at-point
+  "a"   'org-agenda
   "b"   'counsel-bookmark
   "c"   'compile
   "d"   'quick-calc
@@ -456,13 +537,17 @@
   ;; "j"
   "k"   'counsel-ag
   "l"   'org-store-link
-  ;; "m" "n" "o" "p" "q"
+  ;; "m" "n" "o"
+  "p"   'projectile-command-map
+  ;; "q"
+
   "r"   'counsel-recentf
   "R"   '(lambda () (interactive)
            (load-file user-init-file))
   "s"   'cycle-ispell-languages
-  ;; "t" "u" "v" "w"
-  "x"   'evil-numbers/dec-at-point)
+  ;; "t" "u" "v" "w" "x"
+  )
+
 
 (general-def
   :prefix "C-h"
@@ -472,13 +557,14 @@
 (general-def
   :prefix "C-x"
   "f"   'counsel-find-file
-  "C-f" 'counsel-find-file)
+  "C-f" 'counsel-find-file
+  "ESC ESC" 'nil)
 
 (general-def
   "C-;"     'evil-repeat-find-char-reverse
   "C-s"     'swiper
   "M-/"     'hippie-expand
-  "<M-tab>" 'company-complete-common-or-cycle
+  "<M-tab>" 'company-complete
   "M-x"     'counsel-M-x)
 
 ;; lower garbage collection threshold
